2c6d4b471a84ef29e5795b03b06db01794e8f5ca
Saleh Hajiyev <70698698+salehhajiyev@users.noreply.github.com>
2024-05-17 15:40:55 +0400

    Delete 01.Install-Dependencies.sh

--- 01.Install-Dependencies.sh
+++
@@ -1,6 +0,0 @@
-#Install Dependencies
-
-apt-get install automake jq libtool libssl-dev make gcc pkg-config git libjansson-dev libmagic-dev -y
-mkdir /usr/share/yara
-cd /usr/share/yara
-wget https://github.com/VirusTotal/yara/archive/refs/tags/v4.2.3.tar.gz
