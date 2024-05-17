1f14ea44fa41877ccb43b332f5fd7e8bc1dfde42
Saleh Hajiyev <70698698+salehhajiyev@users.noreply.github.com>
2024-05-17 15:16:33 +0400

    Create 04.download-file-change-permission.sh

---
+++ 04.download-file-change-permission.sh
@@ -0,0 +1,4 @@
+cd /usr/share/yara/
+git clone https://github.com/salehhajiyev/yara-wazuh-integration/yara_update_rules.sh
+chmod +x /usr/share/yara/yara_update_rules.sh
+/usr/share/yara/yara_update_rules.sh
